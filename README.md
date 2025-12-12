c0ea65c08372a4a50e7ca234046906643b7a609e
luciaaroca <lucia.aroca2510@gmail.com>
2025-12-12 17:42:17 +0100

    create/tables

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
-# Desafio de tripulaciones
+# Descripción del desafío
 
-Aplicación web completa pero completa completa
+## Configurar .env
+
+Backend
+
+- PG_USER=
+- PG_HOST =
+- PG_DATABASE=
+- PG_PASSWORD=
+- PG_PORT=
+- JWT_SECRET=
+- PG_SSL=
+
+- PORT=
+
+- NODE_ENV=
+
+Frontend
+- VITE_API_URL=
+
+
+
+## Para arrancar el proyecto
+
+```bash
+cd backend
+npm install
+```
+
+```bash
+cd frontend
+npm install
+```
+```bash
+cd ..
+npm install
+```
+```bash
+npm run dev (arrancamos Backend + Frontend)
+```
+```bash
+cd backend
+npm run dev (arrancamos solo backend)
+```
+```bash
+cd frontend
+npm run dev (arrancamos solo frontend)
+```
+
+
+
+
--- backend/queries.sql
+++ backend/queries.sql
@@ -1,57 +1,62 @@
-/* Crear tabla de usuarios */
+/*CREACIÓN TABLAS*/
+/*Employees*/
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    position VARCHAR(100),
+    department VARCHAR(100),
+    salary NUMERIC(10,2)
+);
 
+/*Users*/
 CREATE TABLE users (
-    id_user SERIAL PRIMARY KEY,
-    name VARCHAR(24) NOT NULL,
-    email VARCHAR(120) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(50),
-    is_logged BOOLEAN DEFAULT FALSE
+  user_id SERIAL NOT NULL PRIMARY KEY,
+  employee_id INT UNIQUE,
+  role VARCHAR(50) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  FOREIGN KEY (employee_id) REFERENCES employees(employee_id) ON DELETE CASCADE
 );
 
-/* Crear tabla de tattoos */
-
-CREATE TABLE tattoos (
-    id_tattoo SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    description TEXT,
-    image VARCHAR(255)
+/*Customers*/
+CREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY,
+    first_name_customer VARCHAR(100) NOT NULL,
+    last_name_customer VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    region VARCHAR(100)
+);
+/*Products*/
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    category VARCHAR(100),
+    unit_price NUMERIC(10,2) NOT NULL
 );
 
-/* Crear tabla de reservas*/
-
-CREATE TABLE booking (
-    id_booking SERIAL PRIMARY KEY,
-    id_user INT NOT NULL,
-    id_tattoo INT NOT NULL,
-    date_booking DATE NOT NULL,
-    hour_booking TIME NOT NULL,
-
-    FOREIGN KEY (id_user) REFERENCES users(id_user)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    FOREIGN KEY (id_tattoo) REFERENCES tattoos(id_tattoo)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+/*Sales*/
+CREATE TABLE sales (
+    sale_id SERIAL PRIMARY KEY,
+    employee_id INTEGER NOT NULL,
+    customer_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    sales_channel VARCHAR(100),
+    quantity INTEGER NOT NULL CHECK (quantity > 0),
+    discount_percentage NUMERIC(5,2),
+    payment_method VARCHAR(50),
+    subtotal NUMERIC(10,2),
+    discount_amount NUMERIC(10,2),
+    total NUMERIC(10,2),
+    hour INTEGER,
+    day INTEGER,
+    month INTEGER,
+    year INTEGER,
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id) ON DELETE CASCADE
+    FOREIGN KEY (customer_id) REFERENCES customers (customer_id) ON DELETE CASCADE
+    FOREIGN KEY (product_id) REFERENCES products (product_id) ON DELETE CASCADE
 );
 
-/* Insertar usuarios de ejemplo*/
-INSERT INTO users (name, email, password, role, is_logged)
-VALUES 
-('Ana Torres', 'ana@example.com', 'hashedpass1', 'admin', FALSE),
-('Luis Romero', 'luis@example.com', 'hashedpass2', 'client', FALSE),
-('Marta Pérez', 'marta@example.com', 'hashedpass3', 'client', TRUE);
 
-/* Insertar tatuajes de ejemplo*/
-INSERT INTO tattoos (name, description, image)
-VALUES
-('Dragon Tribal', 'Tatuaje grande estilo tribal', 'dragon.png'),
-('Flor Minimalista', 'Flor pequeña en línea fina', 'flower.png'),
-('Geometría Sagrada', 'Diseño geométrico simétrico', 'geo.png');
 
-/* Insertar reservas de ejemplo*/
-INSERT INTO booking (id_user, id_tattoo, date_booking, hour_booking)
-VALUES
-(1, 2, '2025-01-15', '10:30:00'),
-(2, 1, '2025-02-10', '16:00:00'),
-(3, 3, '2025-03-05', '12:45:00');
+/*INSERT INTO*/
